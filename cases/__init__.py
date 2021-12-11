c02d370d884427ded0f6f9d97f3fccddf1ac4f0c
MohammadAmin Vahedinia <mohammadamin.vahedinia@cafebazaar.ir>
2021-12-11 18:46:19 +0330

    Add test-cases

---
+++ cases/__init__.py
@@ -0,0 +1,6 @@
+from .base import *
+from .case1 import case_1
+from .case2 import case_2
+from .case3 import case_3
+from .case4 import case_4
+from .case5 import case_5
---
+++ cases/base.py
@@ -0,0 +1,10 @@
+class Case:
+    def __init__(self, L, req, ans) -> None:
+        self.L = L
+        self.req = req
+        self.ans = ans
+
+
+Debugger1 = Case(30, [13, 16, 24, 16, 11, 14], 4)
+Debugger2 = Case(100, [48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30], 4)
+Debugger3 = Case(100, [16, 34, 49, 12, 43, 44, 34, 23, 33, 40, 55, 51], 5)
